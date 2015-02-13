1408927bd7fc9b8f763648c3af77cb35161c2e22
Ian Whitney <whit0694@umn.edu>
2015-02-13 14:32:38 -0600

    Initial

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.0'
+gem 'rails-api', '0.4.0'
+
+#gem 'sqlite3'
+
+group :development, :test do
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+
+  gem 'rspec', '~> 3.2'
+  gem 'rspec-rails', '~> 3.2'
+end
+
+group :development do
+  gem 'capistrano', '~> 3.3'
+  gem 'capistrano-rails', '~> 1.1'
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,16 @@
+role :app, %w{asr-course-staging.oit.umn.edu}
+role :web, %w{asr-course-staging.oit.umn.edu}
+
+# Configuration
+set :user, 'swadm'
+set :server, 'asr-course-staging.oit.umn.edu'
+set :roles, %w{web app}
+set :web_root, '/swadm/www/courses-staging.umn.edu'
+
+server 'asr-course-staging.oit.umn.edu',
+  roles: fetch(:roles),
+  web_root: fetch(:web_root),
+  ssh_options: {
+    user: fetch(:user),
+    forward_agent: true,
+    auth_methods: %w(publickey)}
