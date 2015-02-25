647a9779f15ecac1689b88c2043db96ca5fdd71e
Ian Whitney <whit0694@umn.edu>
2015-02-25 17:51:09 -0600

    Setting unique when creating the field does nothing

--- db/migrate/20150225190220_add_campus.rb
+++ db/migrate/20150225190220_add_campus.rb
@@ -1,7 +1,7 @@
 class AddCampus < ActiveRecord::Migration
   def change
     create_table :campuses do |t|
-      t.string :abbreviation
+      t.string :abbreviation, :unique => true
     end
     add_index :campuses, :abbreviation, :unique => true
   end
