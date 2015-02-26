05bb54c0b8953cc336f9adcab088a93e15d0c8de
Ian Whitney <whit0694@umn.edu>
2015-02-26 13:12:43 -0600

    Testing of CampusEntity
    Part of the testing includes a change to how the CampusEntity determines
    if it is valid.

--- app/models/entities/campus_entity.rb
+++ app/models/entities/campus_entity.rb
@@ -13,7 +13,7 @@ module Entities
     end
 
     def valid?(repository)
-      repository.where(abbreviation: abbreviation).empty?
+      repository.unique?(abbreviation: abbreviation)
     end
 
     def attributes
