88cb02f36c5ddb66edab25426e9f2fafe07219b4
Ian Whitney <whit0694@umn.edu>
2015-02-27 09:28:45 -0600

    turn on rspec test focus behavior

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,15 +40,15 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
-=begin
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
   # get run.
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
+# The settings below are suggested to provide a good initial experience
+# with RSpec, but feel free to customize to your heart's content.
+=begin
 
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
