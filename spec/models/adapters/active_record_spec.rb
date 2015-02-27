14a96108285970379d81aeb4fc4c7309f4c6389c
Ian Whitney <whit0694@umn.edu>
2015-02-27 13:39:52 -0600

    Move and rename the shared adapter spec to stop warnings

--- spec/models/adapters/active_record_spec.rb
+++ spec/models/adapters/active_record_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 require_relative "../../../app/models/adapters/active_record"
-require_relative "../../support/shared/adapter_spec"
+require_relative "../../shared/adapter"
 
 RSpec.describe Adapters::ActiveRecord do
   it_behaves_like "an adapter"
