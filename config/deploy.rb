1b5a788644b912bb5e4fa9a31015964a361a2235
Ian Whitney <whit0694@umn.edu>
2015-02-27 10:39:42 -0600

    Update Capistrano deployment for Oracle & Production
    This is a bunch of small fixes that enable capistrano deployment for
    both Staging and Production. It also adds Oracle & ActiveRecord.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 
 set :linked_dirs, %w(bin log tmp/cache vendor/bundle)
+set :linked_files, %w{config/database.yml config/initializers/environment_variables.rb}
 
 namespace :deploy do
   desc 'Restart application'
