1014b7dbb16198bb4a798855c18c0351d23c6b9f
Ian Whitney <whit0694@umn.edu>
2015-03-03 21:22:15 -0600

    Add term seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 Persisters::ActiveRecord::Campus.delete_all
+Persisters::ActiveRecord::Term.delete_all
 @campus_repo = Repositories::CampusRepository.new
 
 %w(UMNTC UMNDL UMNMO UMNRC UMNRO).each do |abbreviation|
   campus = @campus_repo.build
   campus.abbreviation = abbreviation
   @campus_repo.save(campus)
+end
+
+%w(1155 1159 1163).each do |strm|
+  repo = Repositories::TermRepository.new
+  term = repo.build(attributes: {strm: strm})
+  repo.save(term)
 end
