931d93b08a6114a33167ec56831de8523f36213d
Ian Whitney <whit0694@umn.edu>
2015-03-09 14:25:00 -0500

    Replace the term active_record spec

--- spec/models/persisters/active_record/term_spec.rb
+++ spec/models/persisters/active_record/term_spec.rb
@@ -1,12 +1,46 @@
 require "rails_helper"
-require_relative "../../../../app/models/persisters/active_record/term"
 
 RSpec.describe Persisters::ActiveRecord::Term do
-  it "uses strm as its primary key" do
-    expect(described_class.primary_key).to eq("strm")
+  let(:term_instance) { described_class.new }
+
+  describe "type" do
+    it "is 'term'" do
+      expect(term_instance.type).to eq("term")
+    end
   end
 
-  it "uses the ActiveRecord adapter" do
-    expect(described_class.orm_adapter).to eq(Hexagram::Adapters::ActiveRecord)
+  describe "strm" do
+    it "can be set" do
+      term_instance.strm = "1149"
+      expect(term_instance.strm).to eq("1149")
+    end
+
+    it "can be set at instantiation" do
+      expect(described_class.new(strm: "1149").strm).to eq("1149")
+    end
+  end
+
+  describe "id" do
+    it "is the strm" do
+      term_instance.strm = "1149"
+      expect(term_instance.id).to eq("1149")
+    end
+  end
+
+  describe "valid?" do
+    it "is valid if the abbrevation is unique" do
+      expect(described_class.new(strm: "1149").valid?).to be_truthy
+    end
+
+    it "is not valid if the abbrevation is not unique" do
+      described_class.new(strm: "1149").save
+
+      duplicate = described_class.new(strm: "1149")
+      expect(duplicate.valid?).to be_falsey
+    end
+
+    it "is not valid if the strm is blank" do
+      expect(described_class.new(strm: "").valid?).to be_falsey
+    end
   end
 end
