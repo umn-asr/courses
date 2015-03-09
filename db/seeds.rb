7290b405be7fb2e8012acd0539ed23bbbf45d686
Ian Whitney <whit0694@umn.edu>
2015-03-09 14:21:38 -0500

    Update seeds to work without repositories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,11 @@
 #
 Persisters::ActiveRecord::Campus.delete_all
 Persisters::ActiveRecord::Term.delete_all
-@campus_repo = Repositories::CampusRepository.new
 
 %w(UMNTC UMNDL UMNMO UMNRC UMNRO).each do |abbreviation|
-  campus = @campus_repo.build
-  campus.abbreviation = abbreviation
-  @campus_repo.save(campus)
+  Persisters::ActiveRecord::Campus.create({abbreviation: abbreviation})
 end
 
-%w(1155 1159 1163).each do |strm|
-  repo = Repositories::TermRepository.new
-  term = repo.build(attributes: {strm: strm})
-  repo.save(term)
+%w(1149 1155 1159 1163).each do |strm|
+  Persisters::ActiveRecord::Term.create({strm: strm})
 end
