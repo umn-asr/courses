e8ac9595078e374cb168c862b9930bbac83160a0
Debbie Gillespie <debbieg@umn.edu>
2015-03-16 17:16:00 -0500

    Removes commented code

--- db/migrate/20150316194234_add_courses_subject_relation.rb
+++ db/migrate/20150316194234_add_courses_subject_relation.rb
@@ -1,7 +1,5 @@
 class AddCoursesSubjectRelation < ActiveRecord::Migration
   def change
-    # add_column :courses, :subject_id, :integer
-    # add_foreign_key :courses, :subjects
     change_table :courses do |t|
       t.belongs_to :subject, index: true
     end
