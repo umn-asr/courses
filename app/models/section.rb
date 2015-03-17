755eaec23eeb35fc4962e552be4e47f69479ca4c
Ian Whitney <whit0694@umn.edu>
2015-03-16 19:05:28 -0500

    Link section to grading_basis

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,10 +1,11 @@
 class Section < ::ActiveRecord::Base
   belongs_to :course
   belongs_to :instruction_mode
+  belongs_to :grading_basis
 
   validates_presence_of :class_number, :number, :component, :course_id
 
-  attr_accessor :grading_basis, :instructors, :meeting_patterns, :combined_sections
+  attr_accessor :instructors, :meeting_patterns, :combined_sections
 
   def type
     "section"
