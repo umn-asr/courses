822d5b025a2e292de1bbfd4069223cc69a4eb1f6
Ian Whitney <whit0694@umn.edu>
2015-03-17 20:01:35 -0500

    Link section to instructor

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,10 +2,11 @@ class Section < ::ActiveRecord::Base
   belongs_to :course
   belongs_to :instruction_mode
   belongs_to :grading_basis
+  has_many :instructors
 
   validates_presence_of :class_number, :number, :component, :course_id
 
-  attr_accessor :instructors, :meeting_patterns, :combined_sections
+  attr_accessor :meeting_patterns, :combined_sections
 
   def type
     "section"
