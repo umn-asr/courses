55f000ccd6520f39fb225678ff18f774eff0125e
Davin Lagerroos <dlagerro@umn.edu>
2015-04-09 12:36:14 -0500

    start of tests for Course.for_campus_and_term

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,6 +3,29 @@ require "rails_helper"
 RSpec.describe Course do
   let(:course_instance) { described_class.new }
 
+  describe ".for_campus_and_term" do
+    let(:campus)  { Campus.create(abbreviation: "UMNTEST") }
+    let(:term)    { Term.create(strm: "2259") }
+    let(:subject) { campus.subjects.where(term_id: term.id).create(subject_id: "TEST", description: "for testing") }
+    let(:courses) {
+                      3.times do
+                        subject.courses.create(course_id: rand(1000..9999).to_s)
+                      end
+                  }
+
+    it "returns courses that match the supplied campus and term" do
+      # expect(Course.for_campus_and_term(campus, term)).to eq(courses)
+    end
+    context "when there are no matches"
+
+      it "returns an empty collection"
+    context "when the campus matches but the term does not"
+      it "does not return the course"
+    context "when the term matches but the campus does not"
+      it "does not return the course"
+
+  end
+
   describe "type" do
     it "is 'course'" do
       expect(course_instance.type).to eq("course")
