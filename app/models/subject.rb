c907800fda92eca8bd1eee9d953cc9640cb70c9f
Debbie Gillespie <debbieg@umn.edu>
2015-04-09 15:25:23 -0500

    Updates subject validation
    Makes sure a subject's uniqueness is defined by the subject_id,
    campus_id, and term_id. Also ensures the existence of campus_id and
    term_id when creating a subject. Updates the test accordingly.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,8 +3,8 @@ class Subject < ::ActiveRecord::Base
   belongs_to :campus
   has_many :courses, dependent: :destroy
 
-  validates_presence_of :subject_id, :description
-  validates_uniqueness_of :subject_id
+  validates_presence_of :subject_id, :description, :campus_id, :term_id
+  validates_uniqueness_of :subject_id, scope: [:campus, :term]
 
   def type
     "subject"
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -10,21 +10,28 @@ RSpec.describe Subject do
   end
 
   describe "valid?" do
-    let(:valid_attributes) { {subject_id: "PHYS", description: "Physics"}}
+    let(:valid_attributes) { {subject_id: "PHYS", description: "Physics", campus_id: rand(1..500), term_id: rand(1..500)}}
 
-    it "is valid if the subject_id is unique" do
-      expect(described_class.new(valid_attributes).valid?).to be_truthy
+    it "is valid if the subject_id is unique for a campus and term" do
+      base_subject = described_class.create(valid_attributes)
+      new_subject_id = described_class.new(valid_attributes.merge({ subject_id: "CSCI" }))
+      new_campus_id = described_class.new(valid_attributes.merge({ campus_id: rand(1000..2000)}))
+      new_term_id = described_class.new(valid_attributes.merge({ term_id: rand(1000..2000)}))
+      expect(base_subject.valid?).to be_truthy
+      expect(new_subject_id.valid?).to be_truthy
+      expect(new_campus_id.valid?).to be_truthy
+      expect(new_term_id.valid?).to be_truthy
     end
 
-    it "is not valid if the subject_id is not unique" do
+    it "is not valid if the subject_id is not unique for a campus and term" do
       described_class.new(valid_attributes).save
 
-      duplicate = described_class.new({subject_id: valid_attributes[:subject_id], description: "#{rand(3)}"})
+      duplicate = described_class.new({subject_id: valid_attributes[:subject_id], description: "#{rand(3)}", campus_id: valid_attributes[:campus_id], term_id: valid_attributes[:term_id]})
       expect(duplicate.valid?).to be_falsey
     end
 
-    it "is not valid if the subject_id or description are blank" do
-      [:subject_id, :description].each do |required_attr|
+    it "is not valid if the subject_id, description, campus_id, or term_id are blank" do
+      [:subject_id, :description, :campus_id, :term_id].each do |required_attr|
         invalid_attributes = valid_attributes.dup
         invalid_attributes.delete(required_attr)
         expect(described_class.new(invalid_attributes).valid?).to be_falsey
