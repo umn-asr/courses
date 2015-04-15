ed66ca95401dab59e74a13a6c445b2121c1b2fd2
Davin Lagerroos <dlagerro@umn.edu>
2015-04-15 14:43:31 -0500

    make shared tmp folder available
    courses needs access to the folder where peoplesoft_course_class_data
    is putting the files that it gerenates, so that it can import them

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,3 +9,10 @@ ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 set :linked_dirs, %w(bin log tmp/cache vendor/bundle)
 set :linked_files, %w{config/database.yml config/initializers/environment_variables.rb}
+
+after "deploy:updated", :link_shared_tmp_folder do
+  # link in the shared tmp folder
+  on roles(:app) do
+    execute "ln -nfs /swadm/tmp #{release_path}/tmp/json_tmp"
+  end
+end
