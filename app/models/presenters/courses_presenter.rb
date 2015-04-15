a14c7f7785db1f98d38197b36ed398ffadb63350
Ian Whitney <whit0694@umn.edu>
2015-04-15 11:38:15 -0500

    Cache courses individually, instead of as a group
    This changes how we cache objects. We have been caching the full
    CoursesPresenter object (and all of its sub-objects) as one big blob.
    There are a couple problems with this.
    
    - It's not optimal for key/value stores like Redis
    - It introduces a performance roadblock.
    
    For example, if a client were running a search that only returns one
    course, our previous code would be forced to retrieve the entire
    CoursesPresenter object. There is no way to just retrieve *some*
    courses.
    
    As is, this code will still retrieve the full CoursesPresenter object,
    but it's the first step towards allowing us to only retrieve the courses
    we care about.
    
    To avoid db hits on Campus and Term, those are now cached separately.
    
    And the value cached under "[campus_id]_[term_id]" is a collection of
    all the cache keys for the courses underneath that campus & term.

--- app/models/presenters/courses_presenter.rb
+++ app/models/presenters/courses_presenter.rb
@@ -2,15 +2,38 @@ class CoursesPresenter
   attr_accessor :campus, :term, :unfiltered_courses, :courses
 
   def self.fetch(campus_id, term_id)
-    unless Rails.cache.exist?("#{campus_id}_#{term_id}")
+    if Rails.cache.exist?("#{campus_id}_#{term_id}")
+      p = self.new
+      p.campus = Rails.cache.read(campus_id)
+      p.term = Rails.cache.read(term_id)
+
+      course_cache_keys = Rails.cache.read("#{campus_id}_#{term_id}")
+
+      p.unfiltered_courses = course_cache_keys.map { |key| Rails.cache.read(key) }
+
+      p
+    else
+      course_cache_keys = []
+
       p = self.new
 
-      p.campus = Campus.where(abbreviation: campus_id.upcase).first
-      p.term = Term.where(strm: term_id).first
+      p.campus = Rails.cache.fetch(campus_id) do
+        Campus.where(abbreviation: campus_id.upcase).first
+      end
 
-      p.unfiltered_courses = Course.for_campus_and_term(p.campus, p.term).map { |x| CoursePresenter.new(x) }
-      Rails.cache.write("#{campus_id}_#{term_id}", p)
+      p.term = Rails.cache.fetch(term_id) do
+        Term.where(strm: term_id).first
+      end
+
+      p.unfiltered_courses = Course.for_campus_and_term(p.campus, p.term).collect do |course|
+        presented_course = CoursePresenter.new(course)
+        course_cache_keys << presented_course.cache_key
+        Rails.cache.write(presented_course.cache_key, presented_course)
+        presented_course
+      end
+
+      Rails.cache.write("#{campus_id}_#{term_id}", course_cache_keys)
+      p
     end
-    Rails.cache.fetch("#{campus_id}_#{term_id}")
   end
 end
