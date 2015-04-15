d7ab6e0d18f30b07d47be0aefb91c6faa94e65e8
Ian Whitney <whit0694@umn.edu>
2015-04-15 13:42:47 -0500

    Add 2-stage cache retrieval
    In testing on our Staging environment, I found that our previous
    approach to caching wasn't good enough for large data sets. The problem
    (mostly) boiled down to us retrieving all courses from the cache before
    doing the filtering.
    
    With a course count around 8000 (which is what is offered in a term at
    UMNTC), cache retrieval took about 25-30 seconds, using either Redis or
    Memcached. With UMNDL (1700 courses), retrieval took about 5 seconds.
    
    This time is largely wasted, since most of those courses will not end up
    being shown. Users of this service will be filtering their results, so
    we should only retrieve the courses that they actually want to see. Of
    course, we still need enough data to perform the filtering.
    
    To do this, CoursePresenter can now build a 'queryable' version of
    itself. It has just enough data to respond to all of the filtering
    methods that we support.
    
    When caching, we now cache both the full version and the queryable
    version of a course. They are stored under separate keys.
    
    When a request comes in, we grab the queryable courses for the requested
    campus/term. We then filter them using the query string. We then take
    the remaining courses and retrieve the full versions from the cache.
    
    I did some testing of this using a full term's worth of data, testing to
    see how many seconds it took to return a single subject's worth of courses.
    
    campus  old approach  new approach
    umndl   5             1.2
    umntc   25-30         3
    
    Response time increases as you request more and more courses, obviously.
    If someone requested 10 subjects worth of courses from UMNTC, that
    response will be slower. But I think that sort of usage will be limited.
    
    This commit fixes #54, more or less. We could still be faster.
    
    Note that the current implementation of this code is terrible.
    Refactoring to come.

--- app/models/presenters/courses_presenter.rb
+++ app/models/presenters/courses_presenter.rb
@@ -1,39 +1,49 @@
 class CoursesPresenter
   attr_accessor :campus, :term, :unfiltered_courses, :courses
 
+  def self.retrieve(courses)
+    courses.collect(&:cache_key).map { |key| Rails.cache.read(key) }
+  end
+
   def self.fetch(campus_id, term_id)
     if Rails.cache.exist?("#{campus_id}_#{term_id}")
-      p = self.new
-      p.campus = Rails.cache.read(campus_id)
-      p.term = Rails.cache.read(term_id)
+      presented_courses = self.new
+      presented_courses.campus = Rails.cache.read(campus_id)
+      presented_courses.term = Rails.cache.read(term_id)
 
-      course_cache_keys = Rails.cache.read("#{campus_id}_#{term_id}")
+      Rails.cache.read("#{campus_id}_#{term_id}_queryable").each_with_object(presented_courses.queryable_courses) { |k, c| c << Rails.cache.read(k) }
 
-      p.unfiltered_courses = course_cache_keys.map { |key| Rails.cache.read(key) }
-
-      p
+      presented_courses
     else
       course_cache_keys = []
+      queryable_cache_keys = []
 
-      p = self.new
+      presented_courses = self.new
 
-      p.campus = Rails.cache.fetch(campus_id) do
+      presented_courses.campus = Rails.cache.fetch(campus_id) do
         Campus.where(abbreviation: campus_id.upcase).first
       end
 
-      p.term = Rails.cache.fetch(term_id) do
+      presented_courses.term = Rails.cache.fetch(term_id) do
         Term.where(strm: term_id).first
       end
 
-      p.unfiltered_courses = Course.for_campus_and_term(p.campus, p.term).collect do |course|
+      presented_courses.unfiltered_courses = Course.for_campus_and_term(presented_courses.campus, presented_courses.term).collect do |course|
         presented_course = CoursePresenter.new(course)
         course_cache_keys << presented_course.cache_key
+        queryable_cache_keys << "#{presented_course.cache_key}_queryable"
         Rails.cache.write(presented_course.cache_key, presented_course)
-        presented_course
+        Rails.cache.write("#{presented_course.cache_key}_queryable", presented_course.queryable)
+        presented_courses.queryable_courses << presented_course.queryable
       end
 
+      Rails.cache.write("#{campus_id}_#{term_id}_queryable", queryable_cache_keys)
       Rails.cache.write("#{campus_id}_#{term_id}", course_cache_keys)
-      p
+      presented_courses
     end
+  end
+
+  def queryable_courses
+    @queryable_course ||= []
   end
 end
