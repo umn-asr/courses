cf5a8c1afa7ac70d3c94f188aee6fcb924522e07
Ian Whitney <whit0694@umn.edu>
2015-04-16 11:11:08 -0500

    Use the new fetch methods to get campus and term

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,6 +3,8 @@ class CoursesController < ApplicationController
     expires_in(48.hours, :public => true)
 
     @courses = CoursesPresenter.fetch(params[:campus_id], params[:term_id])
+    campus = Campus.fetch(params[:campus_id])
+    term = Term.fetch(params[:term_id])
 
     query_string_search = params[:q]
     filtered_courses = QueryStringSearch.new(@courses.queryable_courses, query_string_search).results
