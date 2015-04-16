9be0df27c8b642b2d3fd20cf900b6e424bc16cc8
Ian Whitney <whit0694@umn.edu>
2015-04-16 13:50:23 -0500

    QueryableCourse.build returns an OpenStruct
    Previous implementation made the full_course an isntance variable of the
    queryable_course, which made it slow to retrieve from the cache. Quick
    cache retrieval is the entire point of this class.

--- app/models/queryable_course.rb
+++ app/models/queryable_course.rb
@@ -1,58 +1,24 @@
 class QueryableCourse
+  attr_accessor :course, :subject_id, :catalog_number, :course_attribute_family, :course_attribute_id, :instruction_mode_id, :locations
+
   def self.read(key)
     Rails.cache.read(key)
   end
 
-  def self.build(course)
-    x = self.new(course)
-    x.course
-    x.subject_id
-    x.catalog_number
-    x.course_attribute_family
-    x.course_attribute_id
-    x.instruction_mode_id
-    x.locations
+  def self.build(full_course)
+    x = self.new
+    x.course = OpenStruct.new(cache_key: full_course.cache_key, id: full_course.id)
+    x.subject_id = full_course.subject.subject_id
+    x.catalog_number = full_course.catalog_number
+    x.course_attribute_family = (full_course.course_attributes.collect { |a| a.family }).to_set
+    x.course_attribute_id = (full_course.course_attributes.collect { |a| a.attribute_id }).to_set
+    x.instruction_mode_id = (full_course.sections.collect { |s| s.instruction_mode.instruction_mode_id}).to_set
+    x.locations = (full_course.sections.collect { |s| s.location}).to_set
     Rails.cache.fetch(x.cache_key) { x }
     x
-  end
-
-  def initialize(full_course)
-    self.full_course = full_course
   end
 
   def cache_key
     "#{course.cache_key}_queryable"
   end
-
-  def course
-    OpenStruct.new(cache_key: full_course.cache_key, id: full_course.id)
-  end
-
-  def subject_id
-    full_course.subject.subject_id
-  end
-
-  def catalog_number
-    full_course.catalog_number
-  end
-
-  def course_attribute_family
-    (full_course.course_attributes.collect { |a| a.family }).to_set
-  end
-
-  def course_attribute_id
-    (full_course.course_attributes.collect { |a| a.attribute_id }).to_set
-  end
-
-  def instruction_mode_id
-    (full_course.sections.collect { |s| s.instruction_mode.instruction_mode_id}).to_set
-  end
-
-  def locations
-    (full_course.sections.collect { |s| s.location}).to_set
-  end
-
-  private
-
-  attr_accessor :full_course
 end
