db0c24d48c9a2ca4c3b9a04e1a75d1c26d13b3ef
Ian Whitney <whit0694@umn.edu>
2015-04-16 11:29:17 -0500

    Update controller to use new objects

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,14 +2,15 @@ class CoursesController < ApplicationController
   def index
     expires_in(48.hours, :public => true)
 
-    @courses = CoursesPresenter.fetch(params[:campus_id], params[:term_id])
     campus = Campus.fetch(params[:campus_id])
     term = Term.fetch(params[:term_id])
 
     query_string_search = params[:q]
-    filtered_courses = QueryStringSearch.new(@courses.queryable_courses, query_string_search).results
+    courses_to_display = QueryStringSearch.new(QueryableCourses.fetch(campus, term), query_string_search).results
 
-    @courses.courses = CoursesPresenter.retrieve(filtered_courses)
+    courses_to_retrieve = courses_to_display.collect(&:course)
+
+    @courses = CoursesPresenter.fetch(campus, term, courses_to_retrieve)
 
     respond_to do |format|
       format.xml
