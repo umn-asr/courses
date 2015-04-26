78ed9e61afb0f62a23ee4e94d06c2d0cd4216103
Ian Whitney <whit0694@umn.edu>
2015-04-26 09:50:58 -0500

    Simplify caching of QueryableCourse(s)
    For some reason I made this more complex than it needs to be. I was
    caching each queryable course individually, and then caching a
    collection of all the queryable course ids. So reading them out of the
    cache required two steps:
    
    1. Retrieve the keys
    2. Retrieve a queryable course for each key
    
    I was probably doing this because of either a speed concern or because
    of the idea that small cache entries are better than big cache entries.
    
    But the speed of this approach is equivalent to just have one big cache
    entry of queryable_courses. And since we always retrieve a collection of
    queryable_courses, we might as well cache them as a collection. Also, it
    makes the code way easier to parse.

--- app/models/queryable_courses.rb
+++ app/models/queryable_courses.rb
@@ -1,18 +1,9 @@
 class QueryableCourses
   def self.fetch(campus, term, cache = Rails.cache)
-    if cache.exist?(cache_key(campus, term))
-      cache.read(cache_key(campus,term)).map do |key|
-        QueryableCourse.read(key)
-      end
-    else
-      queryable_cache_keys = []
-
+    cache.fetch(cache_key(campus, term)) do
       Course.for_campus_and_term(campus, term).collect do |course|
-        queryable_cache_keys << QueryableCourse.build(course).cache_key
+        QueryableCourse.new(course)
       end
-
-      cache.write(cache_key(campus, term), queryable_cache_keys)
-      fetch(campus, term, cache)
     end
   end
 
