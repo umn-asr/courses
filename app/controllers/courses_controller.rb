b2d28e0ccd90639863d722da462af416b022f805
Ian Whitney <whit0694@umn.edu>
2015-04-29 13:36:39 -0500

    Return 404 if url has invalid campus/term
    Fixes #60
    
    This fix is not my favorite, but I'm hard pressed to think of a reason
    why we need something else. As this is the only place in the application
    where we have to ask about the null-ness of the Campus & Term, we might
    as well just do it in a conditional.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,9 +5,13 @@ class CoursesController < ApplicationController
     campus = Campus.fetch(params[:campus_id])
     term = Term.fetch(params[:term_id])
 
-    query_string_search = params[:q]
-    courses_to_retrieve = QueryStringSearch.new(QueryableCourses.fetch(campus, term), query_string_search).results.collect(&:course)
-    content = CoursesPresenter.fetch(campus, term, courses_to_retrieve)
-    render params[:format].to_sym => Serializer.serialize(content, params[:format])
+    if campus && term
+      query_string_search = params[:q]
+      courses_to_retrieve = QueryStringSearch.new(QueryableCourses.fetch(campus, term), query_string_search).results.collect(&:course)
+      content = CoursesPresenter.fetch(campus, term, courses_to_retrieve)
+      render params[:format].to_sym => Serializer.serialize(content, params[:format])
+    else
+      render nothing: true, status: 404
+    end
   end
 end
