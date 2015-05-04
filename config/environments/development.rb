ae5b33ae02723341d758e64ddb46422dac62b7da
Davin Lagerroos <dlagerro@umn.edu>
2015-05-03 23:44:24 -0500

    set base host for url helpers
    RackCacheManager uses url helpers to figure out which urls to
    pre-cache. The url helpers require that the host portion of the url
    is configured, but production and staging did not have these set, so
    the RackCacheManager blew up when it tried to generate the urls to
    cache.
    
    configure the host default_url_options for production, staging and
    development. (Test was already configured.)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.caching.use = false
+
+  routes.default_url_options[:host]="localhost"
+  routes.default_url_options[:port]="3000"
+
 end
