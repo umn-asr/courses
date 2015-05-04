162e20779048583e06faa2b01390893af1d73f82
Davin Lagerroos <dlagerro@umn.edu>
2015-05-04 11:37:01 -0500

    Revert "set base host for url helpers"
    This reverts commit ae5b33ae02723341d758e64ddb46422dac62b7da.
    
    we are going to handle this through an initializer and evironment
    variable rather than hard coding this in each environment file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,6 +66,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  routes.default_url_options[:host] = 'courses-staging.umn.edu'
 end
