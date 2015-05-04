6f0b31df871aa77548b238ce02f78f338539a6e0
Davin Lagerroos <dlagerro@umn.edu>
2015-05-03 22:22:40 -0500

    update schedule.rb to use new import job
    whenever was scheduling the old non-cache refreshing version of the
    import rake task. update schedule.rb to use the version of the
    task that also rebuilds the cache.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 env :MAILTO, 'asr-web-errors@lists.umn.edu'
 
 every 1.day, :at => '1:30 am' do
-  rake "json_import:directory_import['tmp/json_tmp']", output: {standard: nil}
+  rake "json_import:update_all['tmp/json_tmp']", output: {standard: nil}
 end
