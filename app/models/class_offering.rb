6f6cc7dc9a1ba0bfa52b3e957aed0238c07b4dda
Ian Whitney <whit0694@umn.edu>
2015-06-15 12:39:07 -0500

    Use set conversion to remove duplicates
    As the comment says, the scope returns 1 row per section, not one row
    per course. I was using `distinct` to remove duplicates, but that fails
    when used on an Oracle database because the Courses table has a CLOB
    field.
    
    The query that distinct generates is
    
    ```
    SELECT DISTINCT COURSES.* FROM....
    ```
    
    And Distinct and CLOBs do not get along.
    
    There might be a way to resolve this with just ActiveRecord query
    methods, but set conversion works just fine.

--- app/models/class_offering.rb
+++ app/models/class_offering.rb
@@ -2,6 +2,8 @@ class ClassOffering < Course
   scope :all_for_campus_and_term, ->(campus, term) { joins(:sections).joins(:subject).where(subjects: { campus_id: campus.id, term_id: term.id }).order('subjects.description, courses.catalog_number') }
 
   def self.for_campus_and_term(campus, term)
-    all_for_campus_and_term(campus,term).distinct
+    #all_for_campus_and_term returns 1 row per section. to_set removes the duplicates.
+    #the ActiveRecord method `distinct` will fail in Oracle because of the campus.description field being a CLOB
+    all_for_campus_and_term(campus,term).to_set
   end
 end
