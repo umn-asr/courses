b0a97ce1302cf1b35ed9b09985f1f06470454069
Ian Whitney <whit0694@umn.edu>
2015-06-15 10:22:21 -0500

    Add collection of QueryableClasses
    Following the QueryableCourses/QueryableCourse pattern, I'm adding
    similar files for Classes.
    
    QueryableClass is an empty child of QueryableCourse. The behavior of the
    two is identical, but I wanted to use a name that wouldn't lead to
    confusion. I thought it would be odd if QueryableClasses created
    QueryableCourse objects.
    
    I've also changed the cache_key used by QueryableCourse so to avoid
    collisions.

--- app/models/class_offering.rb
+++ app/models/class_offering.rb
@@ -1,3 +1,7 @@
 class ClassOffering < Course
-  scope :for_campus_and_term, ->(campus, term) { joins(:sections).joins(:subject).where(subjects: { campus_id: campus.id, term_id: term.id }).order('subjects.description, courses.catalog_number') }
+  scope :all_for_campus_and_term, ->(campus, term) { joins(:sections).joins(:subject).where(subjects: { campus_id: campus.id, term_id: term.id }).order('subjects.description, courses.catalog_number') }
+
+  def self.for_campus_and_term(campus, term)
+    all_for_campus_and_term(campus,term).distinct
+  end
 end
