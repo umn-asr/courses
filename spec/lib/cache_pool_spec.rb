fdd0bcb0b22f48e64469df3c69b3e66e6cca4c4d
Davin Lagerroos <dlagerro@umn.edu>
2016-01-27 12:19:15 -0600

    Gemify cache_pool
    The old implementation of CachePool had its configuration hardcoded.
    This seemed necessary because we needed CachePool to have its
    configuration loaded prior to the environment (e.g. staging.rb,
    production.rb) getting loaded. The most common way to handle this
    - initializers - do not work by default, because they get loadedi
    after the environment files.
    
    The hardcoding got the job done but once we wanted to change the
    cache pool configuration, it was confusing to remember where to go
    to change it. Since CachePool was written, I've learned more about
    railties which allow us to change things in the rail loading process,
    as long as they get loaded early enough in the rails startup.
    
    Of two two options I came up with - requiring the railtie in
    application.rb file or treating CachePool and its railtie like a gem,
    I've chosen the latter. It requires a few more files, but it is a more
    common patterni. Also the fewer customizations we have in
    application.rb, the easier it is to upgrade rails (we can take
    advantage of the rails update rake task and have less diffing to do).
    
    This commit adds just enough (some folders, a gemspec and a Gemfile)
    to make the CachePool and its railtie load like gem. I added tests for
    the configuration behavior but have left them for now in the main
    project's spec folder. The behavior of CachePool is still tied to rails
    and testing them in the context of a rails application is easier. Plus
    this 'gem' is not ready for prime time yet.

--- spec/lib/cache_pool_spec.rb
+++ spec/lib/cache_pool_spec.rb
@@ -1,75 +1,103 @@
 require 'rails_helper'
 
-RSpec.describe CachePool do
+RSpec.describe CachePool::CachePool do
 
-  let(:index)       { Redis.new }
-  let(:pool_size)   { 7 }
+  describe "configuration" do
+    let(:random_pool_size) { rand(100..999) }
+    let(:random_redis_configuration) { {url: "redis://some_#{rand(999)}_url" } }
 
-  subject { described_class.instance }
-
-  describe "current" do
-    it "returns the same cache when called multiple times" do
-      expect(subject.current).to eq(subject.current)
+    before do
+      Rails.configuration.cache_pool.pool_size           = random_pool_size
+      Rails.configuration.cache_pool.redis_configuration = random_redis_configuration
+      Singleton.__init__(CachePool::CachePool)
     end
 
-    it "has an expiration of 48 hours" do
-      expect(subject.current.options[:expires_in]).to eq(Rails.configuration.caching.default_expiry)
+    it "gets pool size from Rails configuration" do
+      expect(described_class.instance.send(:pool_size)).to eq(random_pool_size)
     end
 
-    context "when current_cache_db is not set in redis" do
-      before do
-        index.del('current_cache_db')
-      end
-
-      it "is the cache store for redis db 1" do
-        expect(subject.current).to respond_to(:fetch)
-        expect(subject.current.options[:db]).to eq(1)
-      end
-    end
-
-    context "when the current_cache_db is set in redis" do
-      let(:cache_number) { rand(1..pool_size) }
-
-      before do
-        index.set('current_cache_db', cache_number)
-      end
-
-      after do
-        index.flushdb
-      end
-
-      it "is cache store for the current_cache_db" do
-        expect(subject.current.options[:db]).to eq(cache_number)
-      end
+    it "gets pool size from Rails configuration" do
+      expect(described_class.instance.send(:redis_configuration)).to eq(random_redis_configuration)
     end
   end
 
-  describe "next" do
-    it "returns the same cache when called multiple times" do
-      expect(subject.current).to eq(subject.current)
+  describe "behavior" do
+    let(:index)               { Redis.new }
+    let(:pool_size)           { rand(3..9) }
+    let(:redis_configuration) { Hash.new }
+
+    subject { described_class.instance }
+
+    before do
+      Rails.configuration.cache_pool.pool_size           = pool_size
+      Rails.configuration.cache_pool.redis_configuration = redis_configuration
+      Singleton.__init__(CachePool::CachePool)
     end
 
-    it "is the cache store for the current db + 1" do
-      current_db = subject.current.options[:db]
-      expect(subject.next.options[:db]).to eq(current_db + 1)
-    end
-
-    context "when the current db is equal to the pool size" do
-      before do
-        index.set('current_cache_db', pool_size)
+    describe "current" do
+      it "returns the same cache when called multiple times" do
+        expect(subject.current).to eq(subject.current)
       end
 
-      it "is the cache store for redis db 1" do
-        expect(subject.next.options[:db]).to eq(1)
+      it "has an expiration of 48 hours" do
+        expect(subject.current.options[:expires_in]).to eq(Rails.configuration.caching.default_expiry)
+      end
+
+      context "when current_cache_db is not set in redis" do
+        before do
+          index.del('current_cache_db')
+        end
+
+        it "is the cache store for redis db 1" do
+          expect(subject.current).to respond_to(:fetch)
+          expect(subject.current.options[:db]).to eq(1)
+        end
+      end
+
+      context "when the current_cache_db is set in redis" do
+        let(:cache_number) { rand(1..pool_size) }
+
+        before do
+          index.set('current_cache_db', cache_number)
+        end
+
+        after do
+          index.flushdb
+        end
+
+        it "is cache store for the current_cache_db" do
+          expect(subject.current.options[:db]).to eq(cache_number)
+        end
       end
     end
-  end
 
-  describe "next!" do
-    it "makes the next store the current store" do
-      old_next = subject.next
-      subject.next!
-      expect(subject.current).to eq(old_next)
+    describe "next" do
+      it "returns the same cache when called multiple times" do
+        expect(subject.current).to eq(subject.current)
+      end
+
+      it "is the cache store for the current db + 1" do
+        current_db = subject.current.options[:db]
+        expect(subject.next.options[:db]).to eq(current_db + 1)
+      end
+
+      context "when the current db is equal to the pool size" do
+        before do
+          index.set('current_cache_db', pool_size)
+        end
+
+        it "is the cache store for redis db 1" do
+          expect(subject.next.options[:db]).to eq(1)
+        end
+      end
+    end
+
+    describe "next!" do
+      it "makes the next store the current store" do
+        old_next = subject.next
+        subject.next!
+        expect(subject.current).to eq(old_next)
+      end
     end
   end
 end
