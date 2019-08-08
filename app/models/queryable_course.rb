96dffc69c3a85eff7f9eb5a4905ef208419d9fee
Ian Whitney <whit0694@umn.edu>
2019-08-08 10:32:35 -0500

    CCF spike

--- app/models/queryable_course.rb
+++ app/models/queryable_course.rb
@@ -1,5 +1,5 @@
 class QueryableCourse
-  attr_accessor :course, :subject_id, :catalog_number, :course_attribute_family, :course_attribute_id, :instruction_mode_id, :locations
+  attr_accessor :course, :subject_id, :catalog_number, :course_attribute_family, :course_attribute_id, :instruction_mode_id, :locations, :location_id
 
   def initialize(full_course)
     self.course = OpenStruct.new(id: full_course.id)
@@ -9,5 +9,12 @@ class QueryableCourse
     self.course_attribute_id = (full_course.course_attributes.collect { |a| a.attribute_id }).to_set
     self.instruction_mode_id = (full_course.sections.collect { |s| s.instruction_mode }.compact.collect { |im| im.instruction_mode_id}).to_set
     self.locations = (full_course.sections.collect { |s| s.location}).to_set
+    self.location_ids = Set.new
+
+    full_course.sections.each do |section|
+      section.meeting_patterns.each do |pattern|
+        location_ids.add(pattern.location_id)
+      end
+    end
   end
 end
